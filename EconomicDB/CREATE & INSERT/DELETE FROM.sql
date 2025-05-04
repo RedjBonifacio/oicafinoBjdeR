2fb8fb9c6515f48b5efef41008bb97a72a0405b7
RedjBonifacio <qrgfbonifacio@tip.edu.ph>
2025-05-04 20:46:31 +0800

    Add files via upload

---
+++ EconomicDB/CREATE & INSERT/DELETE FROM.sql
@@ -0,0 +1,12 @@
+USE ecoDB;
+
+DELETE FROM Policy_Inflation;
+DELETE FROM Business_Policy;
+DELETE FROM Business_Program;
+DELETE FROM Job_Creation_Program;
+DELETE FROM Business;
+DELETE FROM Policy;
+DELETE FROM Inflation_Measure;
+DELETE FROM Sector;
+DELETE FROM Country;
+DELETE FROM Government;
